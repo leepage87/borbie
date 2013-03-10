d247456b747244d30859deeb1e10ef253fe5844b
Richard Teammco <teammcr192@santanoni.(none)>
2013-03-10 17:20:07 -0400

    added building class definition

---
+++ src/buildings.cpp
@@ -0,0 +1,53 @@
+/*	File: buildings.cpp
+ *	Authors: idk your names
+ *
+ *	Description: this file contains the class declaration of the
+ *	buildings list object. The building list object contains a
+ *	function for constructing all buildings and placing them into the
+ *	scene automatically.
+ */
+
+#include "buildings.h"
+
+using namespace irr;
+using namespace scene;
+using namespace video;
+
+
+Buildings::Buildings(
+	ISceneManager *smgr,
+	IVideoDriver *driver)
+{
+	this->smgr = smgr;
+	this->driver = driver;
+}
+
+// delete all of the buildings
+Buildings::~Buildings(){
+	for(std::vector<BuildingInstance *>::iterator it = buildingList.begin();
+		it != buildingList.end(); ++it)
+	{
+		delete (*it);
+	}
+}
+
+
+void Buildings::makeBuilding(unsigned int type,
+		float xPos, float yPos, float zPos)
+{
+	// create texture, w, h, d based on building type
+	ITexture *texture = this->driver->getTexture("assets/textures/building1.png");
+	float width = 100.0f;
+	float height = 100.0f;
+	float depth = 100.0f;
+	
+	BuildingInstance *newBuilding
+		= new BuildingInstance(
+			this->smgr,
+			width, height, depth,
+			xPos, yPos, zPos,
+			texture
+		);
+	
+	this->buildingList.push_back(newBuilding);
+}
--- src/buildings.h
+++ src/buildings.h
@@ -1,15 +1,18 @@
-/*	File: buildingInstance.h
+/*	File: buildings.h
  *	Authors: idk your names
  *
  *	Description: this file contains the class declaration of the
- *	buildings list object. The building object provides a specification
- *	of the abstract GameObject class and contains functions and variables
- *	specific to destructable buildings placed within the environment.
+ *	buildings list object. The building list object contains a
+ *	function for constructing all buildings and placing them into the
+ *	scene automatically.
  */
 
 #ifndef BUILDINGS_H
 #define BUILDINGS_H
 
+
+// include cpp standard library objects
+#include <vector>
 
 // include irrlicht
 #include <irrlicht.h>
@@ -18,10 +21,27 @@
 #include "buildingInstance.h"
 
 
+// types of buildings
+enum BuildingTypes {
+	BUILDING1 = 1
+};
+
+
 // Buildings class:
 class Buildings {
 
+  private:
+	irr::scene::ISceneManager *smgr;
+	irr::video::IVideoDriver *driver;
+	
+	std::vector<BuildingInstance *> buildingList;
+  
   public:
+	Buildings(
+		irr::scene::ISceneManager *smgr,
+		irr::video::IVideoDriver *driver);
+	~Buildings();
+	
 	void makeBuilding(unsigned int type,
 		float xPos, float yPos, float zPos);
 	
