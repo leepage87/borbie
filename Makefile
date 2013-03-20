4270aa99ff0e35e5b1f8a255c2e868dbc8dc2bc8
Richard Teammco <teammcr192@algonquin.(none)>
2013-03-20 12:47:30 -0400

    Added fmod library and compiled with linking it (and including headers)

--- Makefile
+++ Makefile
@@ -20,11 +20,11 @@ OS_SIZE = $(shell uname -m | sed -e "s/i.86/32/" -e "s/x86_64/64/")
 CPPFLAGS = -DROOT_DIR=.
 CXXFLAGS = -g -O3 -ffast-math
 # >>>>> Where to look for for headers
-INCLUDES += -Iinclude/irrlicht
+INCLUDES += -Iinclude/irrlicht -Iinclude/fmod
 
 # >>>>> Where to look for for libs
-# Local
-LDFLAGS += -Llib/$(OS_SIZE) -lIrrlicht
+# Local Irrlicht and FMOD libraries
+LDFLAGS += -Llib/$(OS_SIZE) -lIrrlicht -lfmodex
 # system
 LDFLAGS += -L/usr/X11R6/lib -lGL -lXxf86vm -lXext -lX11 -lXcursor
 
---
+++ src/audioSystem.h
@@ -0,0 +1,29 @@
+/*	File: audioSystem.h
+ *	Authors: idk your names
+ *
+ *	Description: this file contains the class declaration for the
+ *	AudioSystem class. This object is responsible for providing an interface
+ *  wrapping the Audio library subsystem. It is responsible for abstracting
+ *  the audio playback functionality within the game, allowing for 2D and 3D
+ *  sounds in the virtual world, such as music or world sound effects.
+ *
+ *  Currently, this class uses the FMOD audio library.
+ */
+
+#ifndef AUDIO_SYSTEM_H
+#define AUDIO_SYSTEM_H
+
+// System includes
+#include "fmod.hpp"
+#include "fmod_errors.h"
+
+
+// AudioSystem Class
+class AudioSystem {
+  //
+
+
+}; // end of AudioSystem class
+
+
+#endif
