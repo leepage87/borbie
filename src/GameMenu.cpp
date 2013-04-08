fd2ada17738f2ec373a6b21c105f089f10b6f90c
Jonathan Miodownik <miodowj195@algonquin.(none)>
2013-04-08 10:20:08 -0400

    Retextured buttons with a new font located in assets and aligned them.

--- src/GameMenu.cpp
+++ src/GameMenu.cpp
@@ -10,15 +10,15 @@ GameMenu::GameMenu(IGUIEnvironment *guienv, IVideoDriver *driver)
   this->guienv=guienv;
   this->driver=driver;
   this->skin= guienv->getSkin();
-  this->font=guienv->getFont("assets/fonts/bigfont.png");
+/*  this->font=guienv->getFont("assets/fonts/bigfont.png");
   if(font)
     this->skin->setFont(font);
-  
+*/
   //create some buttons!
-  startButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, NEW_GAME, L"Let's Hit the Town!", L"starts a new game");
-  quitButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, QUIT, L"Quit", L"Running Away!?");
-  creditsButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, CREDITS, L"Credits", L"learn about the losers who made this game");
-  scoresButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, HIGH_SCORES, L"High Scores", L"see who's better than you"); 
+  startButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, NEW_GAME, L"", L"starts a new game");
+  quitButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, QUIT, L"", L"Running Away!?");
+  creditsButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, CREDITS, L"", L"learn about the losers who made this game");
+  scoresButton = guienv->addButton(rect<s32>(10,240,110,240 + 32), 0, HIGH_SCORES, L"", L"see who's better than you"); 
 
 
   update();
@@ -52,34 +52,44 @@ void GameMenu::drawMenu()
   float hudH = screenHeight / 8;
 
   //set the positions of the buttons
-  startButton->setRelativePosition(rect<s32>(20, 20, 400, 70));
+  startButton->setRelativePosition(rect<s32>(20, 20, 660, 70));
+  startButton->setImage(driver->getTexture("assets/textures/buttons/NEW_GAME.png"));
+  startButton->setUseAlphaChannel(true);
   startButton->setDrawBorder(0);
-  quitButton->setRelativePosition(rect<s32>(20, 230, 100, 280));
+  quitButton->setRelativePosition(rect<s32>(20, 230, 660, 280));
   quitButton->setDrawBorder(0);
-  scoresButton->setRelativePosition(rect<s32>(20, 90, 240, 140));
+  quitButton->setImage(driver->getTexture("assets/textures/buttons/QUIT.png"));
+  quitButton->setUseAlphaChannel(true);
+  scoresButton->setRelativePosition(rect<s32>(20, 90, 660, 140));
   scoresButton->setDrawBorder(0);
-  creditsButton->setRelativePosition(rect<s32>(20, 160, 160, 210));
-  creditsButton->setDrawBorder(0);
+  scoresButton->setImage(driver->getTexture("assets/textures/buttons/HIGH_SCORES.png"));
+  scoresButton->setUseAlphaChannel(true);
   
- 
-/*
-    // the hud background
+  creditsButton->setRelativePosition(rect<s32>(20, 160, 660, 210));
+  creditsButton->setDrawBorder(0);
+  creditsButton->setImage(driver->getTexture("assets/textures/buttons/CREDITS.png"));
+  creditsButton->setUseAlphaChannel(true);
+
+
+
+  /*
+  // the hud background
   driver->draw2DImage(
-      this->menuTexture,
-      rect<s32>(hudX, hudY, hudX+hudW, hudY+hudH),
-      rect<s32>(0, 0,
-        this->menuTextureSize.Width,
-        this->menuTextureSize.Height));
+  this->menuTexture,
+  rect<s32>(hudX, hudY, hudX+hudW, hudY+hudH),
+  rect<s32>(0, 0,
+  this->menuTextureSize.Width,
+  this->menuTextureSize.Height));
 
   // draw text
   if(font){
-    font->draw(
-        "Borbie's Big Adventure",
-        rect<s32>(hudX, hudY, hudX+hudW, hudY+hudH),
-        SColor(255, 0, 0, 255),
-        true, true); 
-    //bool  	hcenter = false,
-    //bool  	vcenter = false,
+  font->draw(
+  "Borbie's Big Adventure",
+  rect<s32>(hudX, hudY, hudX+hudW, hudY+hudH),
+  SColor(255, 0, 0, 255),
+  true, true); 
+  //bool  	hcenter = false,
+  //bool  	vcenter = false,
   }
   */
 }
