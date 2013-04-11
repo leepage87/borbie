0bcdc438b52f9d00c5543ad99f33e4a554de15db
Lee Page <pagel194@potsdam.edu>
2013-04-11 16:24:37 -0400

    we have a fucked up retarded ass enemy soldier who does nothing

--- src/enemy.h
+++ src/enemy.h
@@ -10,8 +10,18 @@
 
 // Enemy class: abstract, extends GameObject
 class Enemy : public GameObject {
-    //
-    //
+	protected:
+	irr::scene::IAnimatedMeshSceneNode *sceneNode;
+	public:
+	Enemy (irr::scene::ISceneManager *smgr,
+				irr::video::IVideoDriver *driver,
+				irr::IrrlichtDevice *device);
+
+	virtual void doDamage(int damage);
+
+	virtual void applyCollision(
+			irr::scene::IMetaTriangleSelector *metaTriSelector
+		);
 }; // end of Enemy class
 
 
