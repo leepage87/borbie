693e044f9365907ab26d106c40514aea405dfaa1
HPC <hpc@hpc-lab1.(none)>
2013-05-02 16:09:54 -0400

    Splash damage from building to building is now 1/10th of what it was before. That way the whole city won't get killed by 1 car.

--- src/gameInstance.cpp
+++ src/gameInstance.cpp
@@ -303,14 +303,14 @@ void GameInstance::applyExplosionDamage(GameObject *explodingObject) {
         float distance = curNode->getPosition().getDistanceFrom(explodePos);
         if(distance <= explosionRadius){
             int damage = explosionDamage; // max damage
-            // if the exploding object is a building, reduce splash damage
-            //  to prevent instant-killing an entire city. That would suck.
-            if(explodingObject->getObjectType() == TYPE_BUILDING)
-                damage = damage / 10;
             if(distance > 400){ // if more than 400 away, scale down damage
                 float scale = (distance-400) / (explosionRadius-400);
                 damage = int(explosionDamage * scale);
             }
+            // if the exploding object is a building, reduce splash damage
+            //  to prevent instant-killing an entire city. That would suck.
+            if(explodingObject->getObjectType() == TYPE_BUILDING)
+                damage = damage / 10;
             buildings->objList[i]->applyDamage(damage);
             std::cout << "Damaged building @distance=" << distance <<
                 " for @damage=" << damage << std::endl;
